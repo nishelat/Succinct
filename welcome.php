a766c0cc360ff890ba5b3ac386af15315827d282
Nishir <nshelat@usc.edu>
2017-01-15 10:23:09 -0800

    Add icons

--- welcome.php
+++ welcome.php
@@ -4,6 +4,7 @@
       <meta charset="utf-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="icon" href="favicon.png"/>
     
       <title>Succinct</title>
       <link rel="stylesheet" href="css/bootstrap.css">
@@ -80,7 +81,7 @@
         cursor:pointer;
         color: white;
 
-        font-family: Futura;
+        font-family: futuracustom;
         font-size:30px;
         padding:13px 32px;
         text-decoration: none;
