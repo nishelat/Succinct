2159ad05850e92f3cfc1bcaa45830d8a96a78e29
Nishir <nshelat@usc.edu>
2017-01-14 20:10:39 -0800

    Made the switch

---
+++ switch.js
@@ -0,0 +1,8 @@
+window.onload=function() {
+
+document.getElementById('startbutton').addEventListener('click', function() {
+	document.getElementById('main').style.display = "none";
+	document.getElementById('inputArea').style.display = "block";
+});
+
+}
