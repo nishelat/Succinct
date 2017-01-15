fd4dc1c5384c925b5d53f5392687587519816c75
melissajwen <wenm@usc.edu>
2017-01-15 03:14:02 -0800

    revamped design of welcome.php, added foreach loop for images

--- index.php
+++ index.php
@@ -71,21 +71,25 @@
       }
 
       #image1 {
+        padding: 5px;
         object-fit: cover;
         width: 200px;
         height: 200px;
       }
       #image2 {
+        padding: 5px;
         object-fit: cover;
         width: 200px;
         height: 200px;
       }
       #image3 {
+        padding: 5px;
         object-fit: cover;
         width: 200px;
         height: 200px;
       }
       #image4 {
+        padding: 5px;
         object-fit: cover;
         width: 200px;
         height: 200px;
@@ -101,78 +105,101 @@
 
 <!-- Input bar and slide -->
 
-      <div id="inputArea" style="animation-name: fadeIn; animation-duration: 0.5s;">
-        
-          <div>
-            <form id="inputform" action="">
-              <textarea class="form-control" rows="2" type="text" placeholder="Enter title" name="tag"></textarea>
-              <input id="button" class="btn btn-success" type="submit" value="Submit">
-            </form>
-          </div>
+<div id="inputArea" style="animation-name: fadeIn; animation-duration: 0.5s;">
+  <div>
+    <form id="inputform" action="">
+      <textarea class="form-control" rows="2" type="text" placeholder="Enter title" name="tag"></textarea>
+      <input id="button" class="btn btn-success" type="submit" value="Submit">
+    </form>
+  </div>
 
-          <div>
-            <form id="inputform" action="">
-              <textarea class="form-control" rows="2" type="text" placeholder="Enter bullet points" name="tagB"></textarea>
-              <input id="buttonB" class="btn btn-success" type="button" value="Submit">
-            </form>
-          </div>
+  <div>
+    <form id="inputform" action="">
+      <textarea class="form-control" rows="2" type="text" placeholder="Enter bullet points" name="tagB"></textarea>
+      <input id="buttonB" class="btn btn-success" type="button" value="Submit">
+    </form>
+  </div>
 
+  <div id="slide" class="containter">
+    <div>
+      <input id="heading" placeholder="Enter title here...">
+    </div>
+  <div id="point"></div>
 
-          <div id="slide" class="containter">
-              <div><input id="heading" placeholder="Enter title here..."></div>
-              <div id="point"></div>
-             <?php
+  <?php
 
-if(!empty($_GET['tag'])) {
-  $url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=eca302393f034b5435dbe39c81103a10&tags=' . urlencode($_GET['tag']) . '&safe_search=1&sort=relevance&format=json&nojsoncallback=1';
-  $json = file_get_contents($url);
-  $array = json_decode($json, true);
+  if(!empty($_GET['tag'])) {
+    $url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=eca302393f034b5435dbe39c81103a10&tags=' 
+           . urlencode($_GET['tag']) . '&safe_search=1&sort=relevance&format=json&nojsoncallback=1';
+    $json = file_get_contents($url);
+    $array = json_decode($json, true);
+
+    $loop_counter = 0;
+    foreach($array['photos']['photo'] as $_array) {
+      if ($loop_counter++ == 4) break;
+      $farm_id = $_array['farm'];
+      $server_id = $_array['server'];
+      $id = $_array['id'];
+      $secret = $_array['secret'];
+
+      $image_source = 'https://farm' . $farm_id . '.staticflickr.com/' . $server_id . '/' . $id . '_' . $secret . '.jpg';
+      echo '<div id="imagewrapper"><img id="image1" style="float: left; margin-top: 10px;"height="275" src=' . $image_source . '>';
+    }
+  }
+
+  /*
   $farm_id = $array['photos']['photo'][0]['farm'];
   $server_id = $array['photos']['photo'][0]['server'];
   $id = $array['photos']['photo'][0]['id'];
   $secret = $array['photos']['photo'][0]['secret'];
+
   $image_source = 'https://farm' . $farm_id . '.staticflickr.com/' . $server_id . '/' . $id . '_' . $secret . '.jpg';
   echo '<div id="imagewrapper"><img id="image1" style="float: left; margin-top: 10px;"height="275" src=' . $image_source . '>';
+
   $farm_id = $array['photos']['photo'][1]['farm'];
   $server_id = $array['photos']['photo'][1]['server'];
   $id = $array['photos']['photo'][1]['id'];
   $secret = $array['photos']['photo'][1]['secret'];
+
   $image_source = 'https://farm' . $farm_id . '.staticflickr.com/' . $server_id . '/' . $id . '_' . $secret . '.jpg';
   echo '<img id="image2" style="float: left; margin-top: 10px;"height="275" src=' . $image_source . '>';
+
   $farm_id = $array['photos']['photo'][2]['farm'];
   $server_id = $array['photos']['photo'][2]['server'];
   $id = $array['photos']['photo'][2]['id'];
   $secret = $array['photos']['photo'][2]['secret'];
+
   $image_source = 'https://farm' . $farm_id . '.staticflickr.com/' . $server_id . '/' . $id . '_' . $secret . '.jpg';
   echo '<img id="image3" style="float: left; margin-top: 10px;"height="275" src=' . $image_source . '>';
+
   $farm_id = $array['photos']['photo'][3]['farm'];
   $server_id = $array['photos']['photo'][3]['server'];
   $id = $array['photos']['photo'][3]['id'];
   $secret = $array['photos']['photo'][3]['secret'];
+
   $image_source = 'https://farm' . $farm_id . '.staticflickr.com/' . $server_id . '/' . $id . '_' . $secret . '.jpg';
   echo '<img id="image4" style="float: left; margin-top: 10px;"height="275" src=' . $image_source . '></div>';
-}
-?>
-          </div>
-          
-      </div>
+  }
+  */
+
+  ?>
+  </div>        
+</div>
 
 <!-- Scripts -->
 
-      <script src="jquery-3.1.1.min.js"></script>
+<script src="jquery-3.1.1.min.js"></script>
 
-
-    </body>
+</body>
 
 <!-- Footer -->
 
-    <footer class="text-center">
+<footer class="text-center">
 
-        <!-- <div>
-          <h5 style="margin-left: 40px;">Created at HackUCI by the team: The Visual Basics</h5>
-        </div> -->
+  <!-- <div>
+    <h5 style="margin-left: 40px;">Created at HackUCI by the team: The Visual Basics</h5>
+  </div> -->
 
-    </footer>
-
+</footer>
 
 </html>
--- welcome.php
+++ welcome.php
@@ -14,16 +14,51 @@
     <style>
       body {
         font-family: Futura;
-        background-color: #dddddd;
+        /*background-color: #dddddd;*/
+        background-image: url("bg.jpg");
+        background-size: cover;
+        background-repeat: no-repeat;
         margin: 20px;
       }
 
       #main {
-        margin-top:180px;
+        /*margin-top: 40px;*/
       }
       #title {
-        font-size: 50px;
+        font-size: 70px;
       }
+
+      /* The container <div> - needed to position the dropdown content */
+    .dropdown {
+      position: relative;
+    }
+
+    .dropdown-content {
+      display: none;
+      position: absolute;
+      background-color: #f9f9f9;
+      min-width: 160px;
+      box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+    }
+
+    .dropdown-content a {
+      color: black;
+      padding: 12px 16px;
+      text-decoration: none;
+      display: block;
+    }
+
+    .dropdown-content a:hover {
+      background-color: #f1f1f1
+    }
+
+    .dropdown:hover .dropdown-content {
+      display: block;
+    }
+
+    .dropdown:hover .dropbtn {
+       background-color: #3e8e41;
+    }
 
       .createPresentation {
         background-color:#7F7979;
@@ -36,7 +71,7 @@
         color: white;
 
         font-family: Futura;
-        font-size:50px;
+        font-size:30px;
         padding:13px 32px;
         text-decoration: none;
         
@@ -72,28 +107,32 @@
         font-size: 14px;
       }
 
-  
-
     </style>
-
-
 
     <body class="text-center">
 
 <!-- Welcome page -->
 
-      <div id="main" class="container">
-          <h1 id="title">Welcome to Succinct</h1>
+<div class="dropdown">
+  <input type = "image" style = "float: left;" src="hamburger.png" width = 40px height = 40px>
+  <div class="dropdown-content">
+    <a href="#">About</a>
+    <a href="#">Forums</a>
+    <a href="#">Contact</a>
+  </div>
+</div>
+<div id="main" class="container">
+  <h1 id="title">Welcome to Succinct</h1>
+</div>
+<div id="startbutton">
+  <a href="index.php"><button class="text-center createPresentation">Click to Get Started!</button></a>
+</div>
 
-          <div id="startbutton">
-              <a href="index.php"><button class="text-center createPresentation">Create presentation</button></a>
-          </div>
-      </div>
+    
 
 <!-- Scripts -->
 
       <script src="jquery-3.1.1.min.js"></script>
-
 
     </body>
 
