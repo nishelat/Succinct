732186fb9bd46618983ebb41d2ac89ec57def579
Nishir <nshelat@usc.edu>
2017-01-14 20:19:10 -0800

    Add the fade to JS

--- switch.js
+++ switch.js
@@ -3,6 +3,7 @@ window.onload=function() {
 document.getElementById('startbutton').addEventListener('click', function() {
 	document.getElementById('main').style.display = "none";
 	document.getElementById('inputArea').style.display = "block";
+	document.getElementById('inputArea').style.animation = "fadeIn 1s";
 });
 
 }
